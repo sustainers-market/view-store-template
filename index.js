dc99a0b2433a498f810ad3d897ead270e9bc757d
Joao <ritterjv@gmail.com>
2019-09-25 20:38:22 -0700

    -

--- index.js
+++ index.js
@@ -1,76 +1,7 @@
-// const viewStore = require("@sustainers/view-store");
+const viewStore = require("@sustainers/view-store");
 
-const server = require("@sustainers/server");
-const db = require("@sustainers/mongodb-database");
-const logger = require("@sustainers/logger");
-const uuid = require("@sustainers/uuid");
-const secret = require("@sustainers/gcp-secret");
-const datetime = require("@sustainers/datetime");
-
-let _viewStore;
-
-const viewStore = async config => {
-  logger.info("CHECKPOINT 11");
-  if (_viewStore == undefined) {
-    logger.info("CHECKPOINT 22");
-    const s = await secret("mongodb");
-    logger.info("CHECKPOINT 33: ", { secret: s });
-    _viewStore = db.init({
-      name: `${process.env.DOMAIN}.${process.env.ID}`,
-      ...config,
-      connection: {
-        urlProtocol: process.env.MONGODB_URL_PROTOCOL,
-        user: process.env.MONGODB_USER,
-        password: s,
-        host: process.env.MONGODB_HOST,
-        database: process.env.MONGODB_DATABASE,
-        parameters: { authSource: "admin" },
-        autoIndex: true,
-        onOpen: () => logger.info("Thank you database."),
-        onError: err => logger.error("Database has errored.", { err })
-      }
-    });
-  }
-  return _viewStore;
-};
-
-const exportLater = async (config, { get, post, put } = {}) => {
-  logger.info("STARTING");
-  const store = await viewStore(config);
-  logger.info("CHECKPOINT 1");
-  server()
-    .get(async (req, res) => {
-      const results = await store.find({
-        ...(get && get(req.params)),
-        ...(req.params.id && { id: req.params.id })
-      });
-      res.send(results);
-    })
-    .post(async (req, res) => {
-      const id = uuid();
-      await viewStore().write({
-        ...(post && post(req.body)),
-        modified: datetime.fineTimestamp(),
-        id
-      });
-    })
-    .put(async (req, res) => {
-      const update = {
-        ...(put && put(req.body)),
-        modified: datetime.fineTimestamp()
-      };
-      await viewStore().write({ query: { id: req.params.id }, update });
-    })
-    .delete(async (req, res) => {
-      await viewStore().remove({ query: { id: req.params.id } });
-    })
-    .listen();
-  logger.info("CHECKPOINT 2");
-};
-
-module.exports = exportLater({
+module.exports = viewStore({
   schema: { name: { type: String } },
   indexes: [],
-  mixins: [],
-  version: 1
+  mixins: []
 });
--- test.js
+++ test.js
@@ -1,3 +1,38 @@
-describe("view store", () => {
-  ///No need for tests yet while we're just stubbing data.
+const { expect } = require("chai");
+
+const request = require("@sustainers/request");
+const gcpToken = require("@sustainers/gcp-token");
+
+const url = "https://dashboard.view-store.service.core.staging.sm.network";
+
+describe("View store", () => {
+  it("should return successfully", async () => {
+    const token = await gcpToken({
+      operation: "dashboard.view-store.service"
+    });
+
+    logger.info("~", { token });
+
+    const response = await request.post(
+      url,
+      {
+        name: "smelly"
+      },
+      {
+        authorization: `Bearer ${token}`
+      }
+    );
+
+    expect(response.statusCode).to.equal(200);
+
+    //eslint-disable-next-line no-console
+    console.log();
+    logger.info("~", { body: JSON.parse(response.body) });
+
+    expect(JSON.parse(response.body.name)).to.equal("smelly");
+  });
+  // it("should return an error if incorrect params", async () => {
+  //   const response = await post(url, {});
+  //   expect(response.statusCode).to.be.at.least(400);
+  // });
 });
