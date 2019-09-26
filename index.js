231cce8679a942209b35632cba51e534135f868e
Joao <ritterjv@gmail.com>
2019-09-26 15:48:22 -0700

    -

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const viewStore = require("@sustainers/view-store");
 
+//eslint-disable-next-line no-console
+console.log("HII: ", process.env.GCP_KMS_SECRET_BUCKET_KEY_LOCATION);
+
 module.exports = viewStore({
   schema: { name: { type: String } },
   indexes: [],
