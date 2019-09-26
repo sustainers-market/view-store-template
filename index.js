c926fcbea4ccc5fdd64a0c0acd67bc065cba2863
Joao <ritterjv@gmail.com>
2019-09-26 15:56:25 -0700

    -

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const viewStore = require("@sustainers/view-store");
 
 //eslint-disable-next-line no-console
-console.log("HII: ", process.env.GCP_KMS_SECRET_BUCKET_KEY_LOCATION);
+console.log("HII: ", process.env.GCP_KMS_SECRET_BUCKET_KEY_RING);
+//eslint-disable-next-line no-console
+console.log("HII2: ", process.env.GCP_KMS_SECRET_BUCKET_KEY_LOCATION);
+//eslint-disable-next-line no-console
+console.log("HII3: ", process.env.GCP_PROJECT);
+//eslint-disable-next-line no-console
+console.log("HII4: ", process.env.GCP_SECRET_BUCKET);
 
 module.exports = viewStore({
   schema: { name: { type: String } },
