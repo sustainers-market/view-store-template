0fbdf96e39fc502deded928da63e39c1596877bb
Joao <ritterjv@gmail.com>
2019-09-27 08:55:29 -0700

    -

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const viewStore = require("@sustainers/view-store");
 
+/**
+ *
+ * How to write a schema -> https://mongoosejs.com/docs/schematypes.html
+ * Correct types, validators, and default values should be used.
+ *
+ * How to write indexes -> https://mongoosejs.com/docs/guide.html#indexes
+ *
+ */
+
 module.exports = viewStore({
   schema: { name: { type: String } },
   indexes: []
