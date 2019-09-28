bd7854b7b7f0227a55419b46b0089ccb23ee7695
Joao <ritterjv@gmail.com>
2019-09-27 21:17:52 -0700

    -

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 const { expect } = require("chai");
 
 const request = require("@sustainers/request");
-const logger = require("@sustainers/logger");
 
 const url = "http://staged:3000";
 
@@ -9,14 +8,10 @@ process.env.NODE_ENV = "staging";
 
 describe("View store", () => {
   it("should return successfully", async () => {
-    logger.info("~", { url });
-
     const response0 = await request.post(url, {
       name: "smelly"
     });
-
     expect(response0.statusCode).to.equal(200);
-
     expect(JSON.parse(response0.body).name).to.equal("smelly");
 
     const id = JSON.parse(response0.body).id;
@@ -24,47 +19,24 @@ describe("View store", () => {
     const response1 = await request.put(`${url}/${id}`, {
       name: "jelly"
     });
-
     expect(response1.statusCode).to.equal(200);
-
-    logger.info("res1: ", { response1 });
-    logger.info("res1 body: ", { body: JSON.parse(response1.body) });
-
     expect(JSON.parse(response1.body).name).to.equal("jelly");
 
     const response2 = await request.get(`${url}/${id}`);
-
-    logger.info("res2: ", { response2 });
-    logger.info("res2 body: ", { body: JSON.parse(response2.body) });
-
     expect(response2.statusCode).to.equal(200);
-
     expect(JSON.parse(response2.body).name).to.equal("jelly");
 
     const response3 = await request.get(`${url}`);
-
-    logger.info("res3: ", { response3 });
-    logger.info("res3 body: ", { body: JSON.parse(response3.body) });
-
     expect(response3.statusCode).to.equal(200);
-
     expect(JSON.parse(response3.body)[0].name).to.equal("jelly");
 
     const response4 = await request.delete(`${url}/${id}`);
-
-    logger.info("res4: ", { response4 });
-    logger.info("res4 body: ", { body: JSON.parse(response4.body) });
-
     expect(response4.statusCode).to.equal(200);
-
     expect(JSON.parse(response4.body).deletedCount).to.equal(1);
 
     const response5 = await request.get(`${url}/${id}`);
-
-    logger.info("res5: ", { response5 });
-    logger.info("res5 body: ", { body: JSON.parse(response5.body) });
-
     expect(response5.statusCode).to.equal(200);
+    expect(response5.body).to.equal("");
   });
   // it("should return an error if incorrect params", async () => {
   //   const response = await request.post(url, {});
