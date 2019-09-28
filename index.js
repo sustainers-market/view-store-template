8f98970a8f9940f424e50b7b05ce47288d0f1bef
Joao <ritterjv@gmail.com>
2019-09-28 14:29:23 -0700

    -

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const viewStore = require("@sustainers/view-store");
 
 /**
  *
+ * TODO: write the view store's schema and specify what it should be indexed by.
  * How to write a schema -> https://mongoosejs.com/docs/schematypes.html
- * Correct types, validators, and default values should be used.
+ * Types and validators should always be specified,
+ * and default values and required flags should be set if intended.
  *
  * How to write indexes -> https://mongoosejs.com/docs/guide.html#indexes
- *
- * Queries to the store without an index will be rejected.
+ * Queries to the store that aren't indexed will be rejected.
  * In other words, full collection scans are not allowed.
  *
  */
